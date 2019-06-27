2950357ac746fe630c2798a392f8c474134183db
Jose Gomez <jose.fgm1995@gmail.com>
2019-06-27 02:09:06 -0500

    cambio de tipo a campos de ingreso de datos

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,7 @@ function configGame() {
     const label = document.createElement('label');
     label.innerHTML = 'Nombre de jugador';
     const input = document.createElement('input');
+    input.type = 'number';
     input.className = 'name';
     div.appendChild(label);
     div.appendChild(input);
